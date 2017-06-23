cf8145ae4d599d353c5f3215518692d303a07998
takkkkk <git@github.com:takkkkk/sample_app.git>
2017-06-23 00:51:48 +0000

    Start twiiter-collection app

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TwitterCollectionApp
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
+
+ENV.update YAML.load_file('config/settings.yml')[Rails.env] rescue {}
